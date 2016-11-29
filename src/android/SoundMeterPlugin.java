ec4b5a85708a83f0f11d10e874b63d78b2bb46db
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2016-11-29 16:54:30 +0800

    completed

--- src/android/SoundMeterPlugin.java
+++ src/android/SoundMeterPlugin.java
@@ -19,6 +19,7 @@ import org.json.JSONObject;
 public class SoundMeterPlugin extends CordovaPlugin {
     public static final String TAG = "SoundMeterPlugin";
     public static Context thisContext;
+    public static SoundMeter soundMeter;
 
     public SoundMeterPlugin() {
     }
@@ -26,18 +27,34 @@ public class SoundMeterPlugin extends CordovaPlugin {
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         SoundMeterPlugin.thisContext = cordova.getActivity().getApplicationContext();
+
+        SoundMeterPlugin.soundMeter = new SoundMeter();
+        SoundMeterPlugin.soundMeter.start();
+
     }
     
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
         if ("getAmplitude".equals(action)) {
-
+            callbackContext.success(SoundMeterPlugin.soundMeter.getAmplitude());
         } else {
             return false;
         }
 
         return true;    
+    }
+
+    public void onResume(boolean multitasking) {
+        SoundMeterPlugin.soundMeter.start();
+    }
+
+    public void onPause(boolean multitasking) {
+        SoundMeterPlugin.soundMeter.stop();
+    }
+
+    public void onDestroy() {
+        SoundMeterPlugin.soundMeter.stop();
     }
 
 }
